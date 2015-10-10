9494319e044d8dbda9cc58ea08437f1dac2a090f
Ian Storz <i@ianstorz.com>
2015-10-10 13:54:51 -0400

    Copyright year range calculation javascript

---
+++ source/javascripts/copyright.js
@@ -0,0 +1,17 @@
+!(function(document, undefined) {
+  var container = document.getElementById('copyright-year');
+  if (!container) { return; }
+
+  var year = Number(container.innerHTML);
+  if (!year || isNaN(year) || year < 2015) {
+    year = 2015;
+    container.innerHTML = "2015";
+  }
+
+  var date = new Date();
+  var currentYear = Number(date.getUTCFullYear());
+  if (year == currentYear) { return; }
+
+  var newInnerHTML = year + " - " + currentYear;
+  container.innerHTML = newInnerHTML;
+})(document);
